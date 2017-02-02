b8fb35cdd698a2f0f3b17a9d5bdd04cd7b6ad1b3
Gerry Raab <raab@technikum-wien.at>
2017-02-02 01:30:15 +0100

    Bewerbungsphrasen

--- locale/de-AT/bewerbung.php
+++ locale/de-AT/bewerbung.php
@@ -3,4 +3,18 @@
 $this->phrasen['bewerbung/sieHabenNochKeinenZugangscode']='Sie haben noch keinen Zugangscode oder Account an der SFU Wien?';
 $this->phrasen['bewerbung/studierenOderArbeitenSieBereits']='Studieren oder arbeiten Sie bereits an der SFU Wien?';
 $this->phrasen['bewerbung/hierUnverbindlichAnmelden']='Hier anmelden';
+
+$this->phrasen['bewerbung/einleitungstext']='Bitte füllen Sie das Formular aus, wählen Sie die gewünschte(n) Studienrichtung(en) und klicken Sie auf "Abschicken".<br>Danach erhalten Sie eine E-Mail mit Zugangscode an die angegebene Adresse.
+		Mit dem Zugangscode können Sie sich jederzeit einloggen und Ihre Daten vervollständigen';
+$this->phrasen['bewerbung/mailtext']='Sehr %4$s %1$s %2$s!<br><br>
+	Vielen Dank für Ihr Interesse an einem Studiengang der '.CAMPUS_NAME.'. <br>
+	Verwenden Sie für Ihre Bewerbung bitte folgenden Link und Zugangscode: <br><br>
+	<a href="'.APP_ROOT.'addons/bewerbung/cis/registration.php?code=%3$s">Link zur Bewerbung</a><br>
+	Zugangscode: %3$s<br><br>
+	Mit freundlichen Grüßen, <br>
+	Medizinische Fakultät der '.CAMPUS_NAME;
+$this->phrasen['bewerbung/accountVorhanden']='Da Sie bereits als InteressentIn bestätigt wurden oder Sie bereits einen Account an der Medizinischen Fakulät der SFU haben, können Sie Ihre Stammdaten nicht mehr ändern. Sollten hier Daten fehlerhaft sein, wenden Sie sich bitte an die zuständige Assistenz.';
+$this->phrasen['bewerbung/erklaerungBewerbungAbschicken']='Wenn Sie alle Daten vervollständigt haben, können Sie Ihre Bewerbung abschicken.';
+$this->phrasen['bewerbung/erfolgreichBeworben']='Sie haben sich erfolgreich für %s an unserer Universität beworben. Weitere Details bezüglich des Aufnahmeprocedere werden Ihnen fristgerecht per Email übermittelt.';
+
 ?>
