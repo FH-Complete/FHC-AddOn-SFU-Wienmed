bafba227959c12e0040e9ea6fb4ce73ed6a62a01
Gerry Raab <raab@technikum-wien.at>
2017-03-03 02:32:02 +0100

    Phrasen Bewerbung

--- locale/de-AT/bewerbung.php
+++ locale/de-AT/bewerbung.php
@@ -21,7 +21,8 @@ $this->phrasen['bewerbung/sieKoennenMaximalXStudiengaengeWaehlen']='Bitte beacht
 
 $this->phrasen['bewerbung/allgemeineErklaerung']='Wir freuen uns, dass Sie sich für das Bachelor-Studium Humanmedizin an der SFU Med interessieren.<br>
 Wenn Sie mehr Informationen benötigen, steht Ihnen unser Studierenden Office unter info@med.sfu.ac.at gerne zur Verfügung.<br><br>Bitte füllen Sie das Formular vollständig aus. Wenn Sie alles ausgefüllt und hochgeladen haben, können Sie mit „Abschließen“ Ihre Bewerbung an unser Studierenden Office schicken.';
-$this->phrasen['bewerbung/berufstaetigkeit']='Allfällige Berufstätigkeit (Bitte beachten Sie, dass unser Studium als Vollzeit-Studium konzipiert ist. Die Angabe Ihrer allfälligen Berufstätigkeit dient rein statistischen Zwecken und ist nicht verpflichtend.)';
+$this->phrasen['bewerbung/berufstaetigkeit']='Allfällige Berufstätigkeit<br><span style="font-size:10pt;">(Bitte beachten Sie, dass unser Studium als Vollzeit-Studium konzipiert ist. Die Angabe Ihrer allfälligen Berufstätigkeit dient rein statistischen Zwecken und ist nicht verpflichtend.)</span>';
+
 
 $this->phrasen['bewerbung/bitteDokumenteHochladen']='Untenstehend finden Sie eine Auflistung der für Ihr Studium an der SFU Med erforderlichen Dokumente. Bitte schicken Sie unbedingt alle Dokumente, die Sie zur Zeit der Bewerbung zur Verfügung haben.<br><br>Darüber hinaus haben Sie auch die Möglichkeit, Dokumente nachzureichen. Klicken Sie dazu das Symbol „Dokument wird nachgereicht“ (Sanduhr) an. Sie haben hier auch die Möglichkeit, eine kurze Begründung einzutragen.';
 
@@ -30,7 +31,7 @@ $this->phrasen['bewerbung/dokumenteZumHochladen']='Erforderliche Dokumente:';
 $this->phrasen['bewerbung/erklaerungBewerbungAbschicken']='Wenn die Angabe Ihrer Daten vollständig ist und Sie alle Dokumente, soweit möglich, hochgeladen haben, schicken Sie bitte Ihre Bewerbung ab.';
 
 $this->phrasen['bewerbung/menuAusbildung']='Schulausbildung';
-$this->phrasen['bewerbung/ausbildung']='Ausbildung zu Ihrer Zugangsvoraussetzung<br><br>Die Daten zu Ihrer Schulausbildung können nachträglich nicht mehr geändert werden. Sollten hier Daten fehlerhaft sein, wenden Sie sich bitte unter info@med.sfu.ac.at an unser Studierenden Office.';
+$this->phrasen['bewerbung/ausbildung']='Ausbildung zu Ihrer Zugangsvoraussetzung<br><span style="font-size:10pt;">Die Daten zu Ihrer Schulausbildung können derzeit nachträglich nicht mehr geändert werden.<br>Sollten hier Daten fehlerhaft sein, wenden Sie sich bitte unter info@med.sfu.ac.at an unser Studierenden Office.</span>';
 
 $this->phrasen['bewerbung/erfolgreichBeworben']='Sie haben sich erfolgreich für das Bachelor – Studium Humanmedizin in der jeweiligen Vertiefungsrichtung an unserer Universität beworben. Weitere Details bezüglich des Aufnahmeprocedere entnehmen Sie dem Antwortmail auf Ihre Bewerbung bzw. weiteren Info-Mails unseres Dekanats.';
 
