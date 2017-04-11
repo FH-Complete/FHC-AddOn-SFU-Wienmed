1a61e4b0337b54855c66e1bdc810edb33b5dd49f
Gerald Raab <raab@technikum-wien.at>
2017-04-11 12:01:27 +0200

    Mailtext für Infomail an Bewerber bei senden der Bewerbung

--- locale/de-AT/bewerbung.php
+++ locale/de-AT/bewerbung.php
@@ -36,5 +36,16 @@ $this->phrasen['bewerbung/erfolgreichBeworben']='Sie haben sich erfolgreich für
 
 $this->phrasen['bewerbung/erklaerungBewerbungAbschicken']='Schicken Sie Ihre Bewerbung ab, wenn Sie alle Daten vervollständigt und alle erforderlichen Dokumente hochgeladen haben.<br><br>Bitte beachten Sie, dass nicht vorliegende Dokumente nachgereicht werden können.';
 
+$this->phrasen['bewerbung/erfolgreichBeworbenMail']='<h3>Danke für Ihre Bewerbung an der SFU Med!</h3>
+<p>
+Wir prüfen Ihre Dokumente auf inhaltliche Richtigkeit und Vollständigkeit. Bei Nicht Vorliegen dieser Voraussetzungen werden wir Sie bezüglich der Richtigstellung kontaktieren. Bis zur Übernahme der Dokumente von unserer Seite (Status „Dokument bestätigt“) haben Sie die Möglichkeit, über Ihr Login Daten zu berichtigen bzw. zu vervollständigen. </p>
+<p>
+Wir weisen darauf hin, dass Sie mit der Online-Bewerbung automatisch für das Auswahlverfahren zwischen 24.07. und 11.08.2017 angemeldet sind. Bitte halten Sie sich diesen Zeitraum für die Testungen frei.<br>
+Zwischen 15.06. und 30.06.2017 werden Sie Ihre konkreten Termine für das Auswahlverfahren erhalten.</p>
+<p>
+Sollten Sie sich entschließen, nicht am Auswahlverfahren teilnehmen zu wollen, ist eine Abmeldung (ausschließlich elektronisch an <a href=\'mailto:med-accounting@sfu.ac.at\'>med-accounting@sfu.ac.at</a>) bis 15.6.2017 möglich. Bei fristgerechter Abmeldung erfolgt die Rückerstattung der Aufnahmegebühr abzüglich einer Bearbeitungsgebühr von EUR 50,00. Bei nicht fristgerechter Abmeldung erfolgt keine Rückerstattung der Aufnahmegebühr.
+</p>
+<p>Weitere Informationen zum Aufnahmeprocedere erhalten Sie fortlaufend per E-Mail.</p>
+<b>Ihr SFU Med - Team</b>';
 
 ?>
