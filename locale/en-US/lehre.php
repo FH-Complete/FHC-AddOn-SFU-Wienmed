690f4df760a3ce80dba765639b8ade76a058cb9b
Gerald Raab <raab@technikum-wien.at>
2017-08-25 17:30:20 +0200

    Phrases for CIS

--- locale/en-US/lehre.php
+++ locale/en-US/lehre.php
@@ -1,4 +1,4 @@
 <?php
 $this->phrasen['lehre/moodleHandbuch']='';
 $this->phrasen['lehre/semesterplan']='Factsheet';
-$this->phrasen['lehre/download']='LV-Unterlagen';
+$this->phrasen['lehre/download']='Coursefiles';
